f900fdf60adb17e2281565cc14bcc352654260f8
Sorfian <sorfian.tkj@gmail.com>
2023-09-16 21:07:16 +0700

    Initial Commit

---
+++ Bintaro Food/Bintaro_FoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bintaro_FoodApp.swift
+//  Bintaro Food
+//
+//  Created by Sorfian on 16/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Bintaro_FoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
